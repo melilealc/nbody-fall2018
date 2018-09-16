8926978b91f75b52b675f239741640680ede599b
melilealc <mcl52@duke.edu>
2018-09-16 18:01:31 -0400

    update: body, and Nbody complete

--- src/Body.java
+++ src/Body.java
@@ -1,4 +1,173 @@
 
 public class Body {
+	private double myXPos;
+	private double myYPos; 
+	private double myXVel; 
+	private double myYVel; 
+	private double myMass; 
+	private String myFileName; 
+	
+	/**
+	 * Create a Body from parameters 
+	 * @param xp initial x position 
+	 * @param yp initial y position 
+	 * @param xv initial x velocity 
+	 * @param yv initial y velocity 
+	 * @param mass of object 
+	 * @param filename of image for object animation
+	 */
+public Body(double xp, double yp, double xv, double yv, double mass, String filename) {
+	myXPos = xp;
+	myYPos = yp;
+	myXVel = xv;
+	myYVel = yv; 
+	myMass = mass;
+	myFileName = filename;
+	
+}
+/**
+ * Copy constructor: copy instance variables from one
+ * body to this body
+ * @param b is used to initialize this body
+ */
+public Body(Body b) {
+	myXPos = b.getX();
+	myYPos = b.getY();
+	myXVel = b.getXVel();
+	myYVel = b.getYVel(); 
+	myMass = b.getMass();
+	myFileName = b.getName();
+}
+/** 
+ * The next 6 methods are getter methods, which are just used to 
+ * return the variable from another method 
+ * to thus have access to it outside that method 
+ * @return myXPos, myYPos, myXVel, myYVel, myMass, myFileName
+ */
+public double getX() {
+	return myXPos;
+}
+public double getY() {
+	return myYPos;
+}
+public double getXVel() {
+	return myXVel; 
+}
+public double getYVel() {
+	return myYVel;
+}
+public double getMass() {
+	return myMass;
+}
+public String getName() {
+	return myFileName;
+}
+
+/**
+ * Return the distance between this body and another 
+ * @param b the other body to which distance is calculated 
+ * @return distance between this body and b 			
+ */
+public double calcDistance(Body b) {
+	double dx = myXPos - b.getX();
+	double dy = myYPos - b.getY();
+	return Math.sqrt(dx*dx + dy*dy);
+}
+/**
+ * Returns the force exerted on this body by body p, 
+ * @param p, the body specified in the parameters 
+ * Calculates force using masses of the two bodies, 
+ * G gravitational constant 
+ * and distance between the two bodies
+ * @return the force using the force formula 
+ */
+public double calcForceExertedBy(Body p) {
+	double G = 6.67 * Math.pow(10, -11);
+	double m1 = myMass;
+	double m2 = p.getMass();
+	double r = calcDistance(p);
+	return G*(m1*m2)/(Math.abs(r*r));
+}
+/**
+ * Returns the force exerted on this body by body p in the X direction, 
+ * @param p, the body specified in the parameters 
+ * Calculates force using masses of the two bodies, 
+ * G gravitational constant 
+ * and distance between the two bodies
+ * @return the force using the force formula 
+ */
+public double calcForceExertedByX(Body p) {
+	double totalForce = calcForceExertedBy(p);
+	double dx = p.getX()- myXPos;
+	double r = calcDistance(p);
+	return totalForce*(dx/r);
+}
+public double calcForceExertedByY(Body p) {
+	double totalForce = calcForceExertedBy(p);
+	double dy = p.getY() - myYPos;
+	double r = calcDistance(p);
+	return totalForce*(dy/r);
+}
+/**
+ * calculates net force exerted on a certain body
+ * by all the bodies in the array- given in the parameter
+ * @param bodies
+ * @return the net force
+ */
+public double calcNetForceExertedByX(Body [] bodies) {
+	double netsum = 0;
+	for(Body b: bodies) {
+		if(! b.equals(this)) {
+			netsum += calcForceExertedByX(b);
+	}
+	}
+	return netsum;
+}
+public double calcNetForceExertedByY (Body [] bodies) {
+	double netsum =0;
+	for(Body b: bodies) {
+		if(! b.equals(this)) {
+			netsum += calcForceExertedByY(b);
+		}
+	}
+	return netsum;
+}
+
+/**
+ * update is a modifier method that updates the values of 
+ * myXVel, myYVel, myXPos, myYPos after calculating their 
+ * correct values using acceleration and and the xforce and yforce
+ * @param deltaT
+ * @param xforce
+ * @param yforce
+ */
+public void update(double deltaT, double xforce, double yforce) {
+	double ax = xforce/ myMass;
+	double ay = yforce/myMass;
+	double nvx = myXVel + (deltaT*ax);
+	double nvy = myYVel + (deltaT*ay);
+	double nx = myXPos + (deltaT*nvx);
+	double ny = myYPos + (deltaT*nvy);
+	myXPos = nx;
+	myXVel = nvx;
+	myYPos = ny;
+	myYVel = nvy; 
+}
+
+/**
+ * The draw method just shows a non-moving image for 
+ * the body in the simulation 
+ * We will call this void method in the for loop 
+ * so that it creates a moving image of the bodies,
+ * using their different positions and velocities
+ */
+public void draw() {
+	StdDraw.picture(myXPos, myYPos, "images/"+myFileName);
+}
 
 }
+
+
+
+
+
--- src/NBody.java
+++ src/NBody.java
@@ -1,8 +1,6 @@
-	
-
 /**
- * @author YOUR NAME THE STUDENT IN 201
- * 
+ * @author Melissa Leal 
+ * :) 
  * Simulation program for the NBody assignment
  */
 
@@ -14,20 +12,19 @@ public class NBody {
 	
 	/**
 	 * Read the specified file and return the radius
-	 * @param fname is name of file that can be open
+	 * @param fname is name of file that can be opened
 	 * @return the radius stored in the file
 	 * @throws FileNotFoundException if fname cannot be open
 	 */
 	public static double readRadius(String fname) throws FileNotFoundException  {
 		Scanner s = new Scanner(new File(fname));
 	
-		// TODO: read values at beginning of file to
-		// find the radius
 		
+		double nBodies = s.nextInt();
+		double radiusUniv = s.nextDouble();
 		s.close();
-		
-		// TODO: return radius read
-		return 0;	
+	
+		return radiusUniv;	
 	}
 	
 	/**
@@ -42,22 +39,22 @@ public class NBody {
 			Scanner s = new Scanner(new File(fname));
 			
 			// TODO: read # bodies, create array, ignore radius
-			int nb = 0; // # bodies to be read
+			int nb = s.nextInt(); // # bodies to be read
+			double rU = s.nextDouble();
+			Body[] arrayBodies = new Body[nb];
 			
 			for(int k=0; k < nb; k++) {
-				
-				// TODO: read data for each body
-				// construct new body object and add to array
+				arrayBodies[k] = new Body(s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.nextDouble(), s.next());
+
 			}
 			
 			s.close();
 			
-			// TODO: return array of body objects read
-			return null;
+			return arrayBodies;
 	}
 	public static void main(String[] args) throws FileNotFoundException{
-		double totalTime = 157788000.0;
-		double dt = 25000.0;
+		double totalTime = Math.pow(10, 9);
+		double dt = 1000000.0;
 		
 		String fname= "./data/planets.txt";
 		if (args.length > 2) {
@@ -74,23 +71,30 @@ public class NBody {
 	
 		for(double t = 0.0; t < totalTime; t += dt) {
 			
-			// TODO: create double arrays xforces and yforces
-			// to hold forces on each body
 			
-			// TODO: loop over all bodies, calculate
-			// net forces and store in xforces and yforces
+			double [] xforces = new double[bodies.length];
+			double [] yforces = new double[bodies.length];
+			for(int i = 0; i < bodies.length; i ++) {
+				xforces[i] = bodies[i].calcNetForceExertedByX(bodies);
+				yforces[i] = bodies[i].calcNetForceExertedByY(bodies);
+			}
 			
-			// TODO: loop over all bodies and call update
-			// with dt and corresponding xforces, yforces values
+			for(int k = 0; k < bodies.length; k++) {
+				bodies[k].update(dt, xforces[k], yforces[k]);
+			}
+			
 			
 			StdDraw.picture(0,0,"images/starfield.jpg");
 			
-			// TODO: loop over all bodies and call draw on each one
+			for(int i = 0; i < bodies.length; i ++) {
+				bodies[i].draw();
+			}
+			
 			
 			StdDraw.show(10);
 		}
 		
-		// prints final values after simulation
+		
 		
 		System.out.printf("%d\n", bodies.length);
 		System.out.printf("%.2e\n", radius);
@@ -102,3 +106,6 @@ public class NBody {
 		}
 	}
 }
+
+
+
